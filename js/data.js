837c3323d0331e0189b6d0d938e2d5128c383561
Павел <pavel.tagiltsef@gmail.com>
2021-07-10 21:11:40 +0300

    Создает массив с тестовыми объектами

---
+++ js/data.js
@@ -0,0 +1,103 @@
+import { getRandomPositiveFloat, getRandomPositiveNumber} from './util.js';
+
+const TYPES = [
+  'palace',
+  'flat',
+  'house',
+  'bungalow',
+];
+
+const CHECKIN_SLOTS = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const CHECKOUT_SLOTS = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
+
+const FEATURES = [
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner',
+];
+
+const PHOTOS = [
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
+];
+
+const MIN_X = 35.65000;
+const MAX_X = 35.70000;
+
+const MIN_Y = 139.70000;
+const MAX_Y = 139.80000;
+
+const MIN_PRICE = 1000;
+const MAX_PRICE = 100000;
+
+const MIN_ROOMS = 1;
+const MAX_ROOMS = 5;
+
+const MIN_GUESTS = 1;
+const MAX_GUESTS = 5;
+
+const NUMBER_OF_OFFERS = 8;
+
+const getRundomLocation = () => {
+  const numbersAfterPoint = 5;
+  const randomX = getRandomPositiveFloat(MIN_X, MAX_X, numbersAfterPoint);
+  const randomY = getRandomPositiveFloat(MIN_Y, MAX_Y, numbersAfterPoint);
+
+  return {
+    x: randomX,
+    y: randomY,
+  }
+}
+
+const addZeroToNumbersLessThanTen = (number) => {
+  return number < 10 ? '0' + number : number;
+}
+
+const getRandomArrayElement = (arr) => {
+  return arr[getRandomPositiveNumber(0, arr.length - 1)];
+}
+
+const getArrayRandomLength = (arr) => {
+  return arr.slice(0, getRandomPositiveNumber(1, arr.length - 1));
+}
+
+const createOffer = function(index) {
+  const rundomLocation = getRundomLocation();
+
+  return {
+    author: {
+      avatar: `img/avatars/user${addZeroToNumbersLessThanTen(index + 1)}`,
+    },
+    offer: {
+      title: 'Загаловок',
+      address: `${rundomLocation.x}, ${rundomLocation.y}`,
+      price: getRandomPositiveNumber(MIN_PRICE, MAX_PRICE),
+      type: getRandomArrayElement(TYPES),
+      rooms: getRandomPositiveNumber(MIN_ROOMS, MAX_ROOMS),
+      guests: getRandomPositiveNumber(MIN_GUESTS, MAX_GUESTS),
+      checkin: getRandomArrayElement(CHECKIN_SLOTS),
+      checkout: getRandomArrayElement(CHECKOUT_SLOTS),
+      features: getArrayRandomLength(FEATURES),
+      description: 'Описание',
+      photos: getArrayRandomLength(PHOTOS),
+      location: rundomLocation,
+    },
+  };
+}
+
+const rundomOffers = new Array(NUMBER_OF_OFFERS).fill(null).map((item, index) => createOffer(index));
+
+export {rundomOffers};
---
+++ js/util.js
@@ -0,0 +1,17 @@
+const getRandomPositiveFloat = (min, max, rounding = 0) => {
+  const lower = Math.min(Math.abs(min), Math.abs(max));
+  const upper = Math.max(Math.abs(min), Math.abs(max));
+
+  const result = Math.random() * (upper - lower) + lower;
+  return result.toFixed(rounding);
+};
+
+function getRandomPositiveNumber (min, max) {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+export {getRandomPositiveFloat, getRandomPositiveNumber};
