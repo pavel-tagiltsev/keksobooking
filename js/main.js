9465e5a8ba4ad8ebc5d256b34bd4effa156a08ca
Павел <pavel.tagiltsef@gmail.com>
2021-07-22 17:20:32 +0300

    Производит рефакторинг в модуле map

--- js/main.js
+++ js/main.js
@@ -1,112 +1,10 @@
-import {debounce} from './util/util.js';
-import {renderPopups} from './modules/render-popups.js';
-import {getData, sendData} from './modules/api.js';
-import {filterPins} from './modules/filter.js';
+import './modules/map.js';
+
+import {sendData} from './modules/api.js';
 import setFormValidation from './modules/set-form-validation.js';
-
-const filter = document.querySelector('.map__filters');
-
-const deleteActivePinas = () => {
-  const activePins = document.querySelectorAll('.leaflet-marker-icon');
-  const shadows = document.querySelectorAll('.leaflet-marker-shadow');
-
-  shadows.forEach(shadow => {
-    shadow.remove();
-  });
-
-  activePins.forEach(pin => {
-    if (!pin.classList.contains('leaflet-marker-draggable')) {
-      pin.remove();
-    }
-  });
-}
+import {showModal} from './modules/show-modal.js';
 
 //Инициализация карты
-const LAT_START = 35.68283;
-const LNG_START = 139.75945;
-
-const renderPins = (arr) => {
-  arr.forEach(offer => {
-    L.marker([offer.location.lat, offer.location.lng]).addTo(map)
-      .bindPopup(renderPopups(offer));
-  });
-}
-
-const getFilterActive = () => {
-  const mainFilter = document.querySelector('.map__filters');
-  mainFilter.classList.remove('map__filters--disabled');
-
-  const disabledFieldsets = document.querySelectorAll('.map__filters fieldset:disabled');
-  const disabledSelects = document.querySelectorAll('.map__filters select:disabled');
-
-  disabledSelects.forEach(element => element.removeAttribute('disabled'));
-  disabledFieldsets.forEach(element => element.removeAttribute('disabled'));
-}
-
-const getFormActive = () => {
-  const disabledFieldsets = document.querySelectorAll('.ad-form fieldset:disabled');
-  const disabledSelects = document.querySelectorAll('.ad-form select:disabled');
-
-  disabledSelects.forEach(element => element.removeAttribute('disabled'));
-  disabledFieldsets.forEach(element => element.removeAttribute('disabled'));
-
-  const mainForm = document.querySelector('.ad-form');
-  mainForm.classList.remove('ad-form--disabled');
-}
-
-const setMainPin = (lat, lng) => {
-  const myIcon = L.icon({
-    iconUrl: '../leaflet/images/main.png',
-    iconSize: [38, 60],
-    iconAnchor: [19, 60],
-  });
-
-  const main = L.marker([lat, lng], {
-    icon: myIcon,
-    draggable: true,
-  });
-
-  const adress = document.querySelector('#address');
-
-  main.on('move', function (evt) {
-    adress.value = `${evt.latlng.lat.toFixed(5)} : ${evt.latlng.lng.toFixed(5)}`;
-  });
-
-  adress.value = `${main._latlng.lat.toFixed(5)} : ${main._latlng.lng.toFixed(5)}`;
-
-  return main;
-}
-
-
-const map = L.map('map-canvas');
-const main = setMainPin(LAT_START, LNG_START);
-
-map.on('load', () => {
-  getFormActive();
-  main.addTo(map);
-
-  getData('https://22.javascript.pages.academy/keksobooking/data', (response) => {
-    renderPins(response);
-    getFilterActive();
-
-    filter.addEventListener('input', debounce(() => {
-      const filteredOffers = filterPins(response);
-      map.closePopup();
-      deleteActivePinas();
-      renderPins(filteredOffers);
-    }, 500));
-  }, () => {
-    showErrorModal('#load-error');
-  });
-});
-
-map.setView([LAT_START, LNG_START], 9);
-
-const layer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-});
-
-layer.addTo(map);
 
 
 // Работа с ограничениями на форме
@@ -115,37 +13,6 @@ setFormValidation();
 //Отправка формы
 
 const form = document.querySelector('.ad-form');
-
-const showModal = (templateSelector, modalSelector) => {
-  const template = document.querySelector(templateSelector);
-  const modalClone = template.content.cloneNode(true);
-  const main = document.querySelector('main');
-
-  main.appendChild(modalClone);
-
-  const modal = document.querySelector(modalSelector);
-
-  const onEscPress = (evt) => {
-    if (evt.key === 'Escape') {
-      modal.remove();
-      window.removeEventListener('keydown', onEscPress);
-      document.body.style.overflow = '';
-    }
-  }
-
-  const onModalClick = (evt) => {
-    if (evt.currentTarget === modal) {
-      modal.remove();
-      modal.removeEventListener('click', onModalClick);
-      document.body.style.overflow = '';
-    }
-  }
-
-  document.body.style.overflow = 'hidden';
-  window.addEventListener('keydown', onEscPress);
-  modal.addEventListener('click', onModalClick);
-  onReset();
-}
 
 form.addEventListener('submit', evt => {
   evt.preventDefault();
@@ -163,25 +30,25 @@ form.addEventListener('submit', evt => {
 
 
 // reset
-const reset = document.querySelector('.ad-form__reset');
+// const reset = document.querySelector('.ad-form__reset');
 
-function onReset() {
-  // price.placeholder = 'мин. 1 000';
-  // price.min = '1000';
+// function onReset() {
+//   price.placeholder = 'мин. 1 000';
+//   price.min = '1000';
 
-  filter.reset();
-  form.reset();
-  // copacityReset();
-  map.closePopup();
-  deleteActivePinas();
-  getData('https://22.javascript.pages.academy/keksobooking/data', (response) => {
-    renderPins(response);
-  });
-  main.setLatLng([LAT_START, LNG_START]);
-}
+//   filter.reset();
+//   form.reset();
+//   // copacityReset();
+//   map.closePopup();
+//   deleteActivePinas();
+//   getData('https://22.javascript.pages.academy/keksobooking/data', (response) => {
+//     renderPins(response);
+//   });
+//   main.setLatLng([LAT_START, LNG_START]);
+// }
 
 
-reset.addEventListener('click', (evt) => {
-  evt.preventDefault();
-  onReset();
-});
+// reset.addEventListener('click', (evt) => {
+//   evt.preventDefault();
+//   onReset();
+// });
---
+++ js/modules/map.js
@@ -0,0 +1,145 @@
+import {debounce} from '../util/util.js';
+import {renderPopups} from './render-popups.js';
+import {getData} from './api.js';
+import {filterPins} from './filter.js';
+import {showModal} from './show-modal.js';
+
+const STARTING_LATITUDE = 35.68283;
+const STARTING_LONGITUDE = 139.75945;
+const STARTING_ZOOM = 9;
+const MAIN_MARKER_WIDTH = 52;
+const MAIN_MARKER_HEIGHT = 52;
+const MARKER_WIDTH = 40;
+const MARKER_HEIGHT = 40;
+const GET_URL = 'https://22.javascript.pages.academy/keksobooking/data';
+
+const map = L.map('map-canvas');
+const layer = L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+);
+const markers = [];
+
+const mapFilter = document.querySelector('.map__filters');
+const adForm = document.querySelector('.ad-form');
+
+const setAdress = (lat, lng) => {
+  const adress = document.querySelector('#address');
+  adress.value = `${lat.toFixed(5)} : ${lng.toFixed(5)}`;
+}
+
+setAdress(STARTING_LATITUDE, STARTING_LONGITUDE);
+
+const removeMarkers = () => {
+  markers.forEach((marker) => {
+    marker.remove();
+  })
+}
+
+const getFormActive = (form) => {
+  const classes = form.classList;
+  const lastFormClass = classes[classes.length - 1];
+  classes.remove(lastFormClass);
+
+  Array.from(form.children).forEach((fieldset) => {
+    fieldset.disabled = false;
+  });
+}
+
+const setMainMarkerOnMap = () => {
+  const mainMarkerIcon = L.icon({
+    iconUrl: '../img/main-marker.svg',
+    iconSize: [MAIN_MARKER_WIDTH, MAIN_MARKER_HEIGHT],
+    iconAnchor: [MAIN_MARKER_WIDTH / 2, MAIN_MARKER_HEIGHT],
+  });
+
+  const mainMarker = L.marker({
+    lat: STARTING_LATITUDE,
+    lng: STARTING_LONGITUDE,
+  }, {
+    icon: mainMarkerIcon,
+    draggable: true,
+  });
+
+  const onMianMarkerMove = (evt) => {
+    const lat = evt.target.getLatLng().lat;
+    const lng = evt.target.getLatLng().lng;
+    setAdress(lat, lng);
+  }
+
+  mainMarker.on('move', onMianMarkerMove);
+  mainMarker.addTo(map);
+}
+
+const addMarkersOnMap = (offers) => {
+  const createMarker = (offer) => {
+    const markerIcon = L.icon({
+      iconUrl: '../img/marker.svg',
+      iconSize: [MARKER_WIDTH, MARKER_HEIGHT],
+      iconAnchor: [MARKER_WIDTH / 2, MARKER_HEIGHT],
+    });
+
+    const marker = L.marker({
+      lat: offer.location.lat,
+      lng: offer.location.lng,
+    }, {
+      icon: markerIcon,
+    });
+
+    marker
+      .addTo(map)
+      .bindPopup(renderPopups(offer));
+
+    return marker;
+  };
+
+  offers.forEach((offer) => {
+    const marker = createMarker(offer);
+    markers.push(marker);
+  });
+};
+
+const onMapFilterChange = (offers) => {
+  const filteredOffers = filterPins(offers);
+  map.closePopup();
+  removeMarkers();
+  addMarkersOnMap(filteredOffers);
+}
+
+const onSuccessMapLoad = (offers) => {
+  addMarkersOnMap(offers);
+  getFormActive(mapFilter);
+  mapFilter.addEventListener('change', debounce(() => onMapFilterChange(offers), 500));
+}
+
+const onFailMapLoad = () => {
+  showModal('#load-error', '.error');
+}
+
+const onMapLoad = () => {
+  getFormActive(adForm);
+  setMainMarkerOnMap();
+  getData(
+    GET_URL,
+    onSuccessMapLoad,
+    onFailMapLoad,
+  );
+}
+
+const setMap = () => {
+  map
+    .on('load', onMapLoad)
+    .setView({
+      lat: STARTING_LATITUDE,
+      lng: STARTING_LONGITUDE,
+    }, STARTING_ZOOM);
+
+  layer.addTo(map);
+};
+
+setMap();
+
+
